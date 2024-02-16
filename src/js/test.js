1e87130fea6ac32a14b75357a4dc4a7cd4998945
Hanin Farhan <haninfarhan96@gmail.com>
2024-02-16 10:23:40 +0100

    test

--- src/js/test.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-let clickBtn = document.getElementById('img-click');
-let imageRotate = document.getElementById('img-rotate');
-
-
-clickBtn.addEventListener('click', () => {
-    imageRotate.classList.toggle('rotate');
-});
